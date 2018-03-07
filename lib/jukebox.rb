f2a18da2e8f177896c78fadf194352b202fc4447
Celeste Gerard <celeste.gerard@gmail.com>
2018-03-07 12:59:57 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -33,8 +33,9 @@ def play(songs)
     if users_song_choice == song || users_song_choice.to_i == (index + 1)
       puts "Playing #{song}"
       break
-      else
+    else
         puts "Invalid input, please try again"
+        break
       end
     end
   end
