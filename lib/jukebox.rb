ba14fc5d2de2341af1bcb2ee8fa773ef9b06194c
Celeste Gerard <celeste.gerard@gmail.com>
2018-03-07 12:46:51 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -29,7 +29,7 @@ def play(songs)
   users_song_choice = gets.chomp
   
   songs.each_with_index do |song, index|
-    if users_song_choice == "#{song}" || "#{index + 1}"
+    if users_song_choice == song || "#{index + 1}"
       puts "Playing #{song}"
       else
         puts "Invalid input, please try again"
